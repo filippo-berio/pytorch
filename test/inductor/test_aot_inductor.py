c10e087b184d182424f7e73229b0f8e8bdb14e03
Bin Bao <binbao@meta.com>
2023-09-16 19:45:19 -0700

    [inductor] Forward fix a windows test error
    Summary: forward fix a windows test error from https://github.com/pytorch/pytorch/pull/109391
    
    ghstack-source-id: b9f26d74035d7b79104463abf7e1288a383b564f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109449

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -1,6 +1,5 @@
 # Owner(s): ["module: inductor"]
 import copy
-import functools
 import unittest
 
 import torch
@@ -12,22 +11,25 @@ from torch._dynamo.testing import same
 from torch._inductor import config
 from torch._inductor.utils import aot_inductor_launcher
 
-from torch.testing._internal.common_utils import IS_FBCODE, TEST_WITH_ROCM, TestCase
+from torch.testing._internal.common_utils import (
+    IS_CI,
+    IS_FBCODE,
+    IS_WINDOWS,
+    TEST_WITH_ROCM,
+    TestCase,
+)
 from torch.testing._internal.inductor_utils import HAS_CUDA
 from torch.utils import _pytree as pytree
 
-aten = torch.ops.aten
-requires_cuda = functools.partial(unittest.skipIf, not HAS_CUDA, "requires cuda")
-
-try:
-    try:
-        from . import test_torchinductor
-    except ImportError:
-        import test_torchinductor
-except unittest.SkipTest:
+if IS_WINDOWS and IS_CI:
+    sys.stderr.write(
+        "Windows CI does not have necessary dependencies for test_torchinductor yet\n"
+    )
     if __name__ == "__main__":
         sys.exit(0)
-    raise
+    raise unittest.SkipTest("requires sympy/functorch/filelock")
+
+from inductor.test_torchinductor import copy_tests
 
 
 class AOTInductorModelRunner:
@@ -519,9 +521,7 @@ class AOTInductorTestABICompatibile(TestCase):
     check_model_with_multiple_inputs = check_model_with_multiple_inputs
 
 
-test_torchinductor.copy_tests(
-    AOTInductorTestsTemplate, AOTInductorTestABICompatibile, "abi_compatible"
-)
+copy_tests(AOTInductorTestsTemplate, AOTInductorTestABICompatibile, "abi_compatible")
 
 
 class AOTInductorTestNonABICompatible(TestCase):
@@ -530,7 +530,7 @@ class AOTInductorTestNonABICompatible(TestCase):
     check_model_with_multiple_inputs = check_model_with_multiple_inputs
 
 
-test_torchinductor.copy_tests(
+copy_tests(
     AOTInductorTestsTemplate, AOTInductorTestNonABICompatible, "non_abi_compatible"
 )
 
